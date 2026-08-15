e13279361b2a6b31d673e5a83a8bb87049c4444c
Yuvraj dev <yuvrajdev05@gmail.com>
2026-08-16 00:51:41 +0530

    Update start.go

--- internal/modules/start.go
+++ internal/modules/start.go
@@ -7,9 +7,10 @@
 package modules
 
 import (
+	"time"
+
 	"github.com/Laky-64/gologging"
 	tg "github.com/amarnathcjd/gogram/telegram"
-	"time"
 
 	"main/internal/config"
 	"main/internal/core"
@@ -17,6 +18,8 @@ import (
 	"main/internal/locales"
 	"main/internal/utils"
 )
+
+const startStickerID = "CAACAgUAAxkBAAEf9h1qRSPKlYwtAAFFrNVl-JWpojGWyT8AAjAJAAKJsAFXMkMs6wG9EwY8BA"
 
 func init() {
 	helpTexts["/start"] = `<i>Start the bot and show main menu.</i>`
@@ -49,20 +52,71 @@ func startHandler(m *tg.NewMessage) error {
 
 	default:
 
-		msg1, _ := m.Respond("🌟 𝑾𝒆𝒍𝒄𝒐𝒎𝒆 𝑻𝒐 𝑳𝒐𝒗𝒆𝒍𝒍𝒚 𝑴𝒖𝒔𝒊𝒄 🌟", nil)
+		// -------------------------------------------------
+		// INNOMUSIC STICKER REPLY
+		// -------------------------------------------------
+		sticker, err := tg.ResolveBotFileID(startStickerID)
+
+		if err != nil {
+			gologging.Error(
+				"[start] Failed to resolve sticker: " + err.Error(),
+			)
+		} else {
+			stickerMsg, err := m.ReplyMedia(
+				sticker,
+				&tg.MediaOptions{
+					NoForwards: true,
+				},
+			)
+
+			if err != nil {
+				gologging.Error(
+					"[start] Failed to send sticker: " + err.Error(),
+				)
+			} else if stickerMsg != nil {
+				time.Sleep(2500 * time.Millisecond)
+
+				if err := stickerMsg.Delete(); err != nil {
+					gologging.Error(
+						"[start] Failed to delete sticker: " + err.Error(),
+					)
+				}
+			}
+		}
+
+		// -------------------------------------------------
+		// START ANIMATION
+		// -------------------------------------------------
+
+		msg1, _ := m.Respond(
+			"🌟 𝑾𝒆𝒍𝒄𝒐𝒎𝒆 𝑻𝒐 𝑳𝒐𝒗𝒆𝒍𝒍𝒚 𝑴𝒖𝒔𝒊𝒄 🌟",
+			nil,
+		)
+
 		time.Sleep(1 * time.Second)
+
 		if msg1 != nil {
 			msg1.Delete()
 		}
 
-		msg2, _ := m.Respond("𓆩🎵𓆪 𝑩𝒆𝒔𝒕 𝑴𝒖𝒔𝒊𝒄 𝑬𝒙𝒑𝒆𝒓𝒊𝒆𝒏𝒄𝒆 𓆩🎵𓆪", nil)
+		msg2, _ := m.Respond(
+			"𓆩🎵𓆪 𝑩𝒆𝒔𝒕 𝑴𝒖𝒔𝒊𝒄 𝑬𝒙𝒑𝒆𝒓𝒊𝒆𝒏𝒄𝒆 𓆩🎵𓆪",
+			nil,
+		)
+
 		time.Sleep(1 * time.Second)
+
 		if msg2 != nil {
 			msg2.Delete()
 		}
 
-		msg3, _ := m.Respond("💖𓆩🎶𓆪 𝑯𝒊𝒈𝒉 𝑸𝒖𝒂𝒍𝒊𝒕𝒚 𝑺𝒕𝒓𝒆𝒂𝒎𝒊𝒏𝒈 𓆩🎶𓆪💖", nil)
+		msg3, _ := m.Respond(
+			"💖𓆩🎶𓆪 𝑯𝒊𝒈𝒉 𝑸𝒖𝒂𝒍𝒊𝒕𝒚 𝑺𝒕𝒓𝒆𝒂𝒎𝒊𝒏𝒈 𓆩🎶𓆪💖",
+			nil,
+		)
+
 		time.Sleep(1 * time.Second)
+
 		if msg3 != nil {
 			msg3.Delete()
 		}
@@ -73,76 +127,119 @@ func startHandler(m *tg.NewMessage) error {
 				ParseMode: "HTML",
 			},
 		)
+
 		time.Sleep(1 * time.Second)
+
 		if msg4 != nil {
 			msg4.Delete()
 		}
 
 		time.Sleep(1 * time.Second)
-		caption := F(m.ChannelID(), "start_private", locales.Arg{
-			"user": utils.MentionHTML(m.Sender),
-			"bot":  utils.MentionHTML(m.Client.Me()),
-		})
 
-		_, err := m.RespondMedia(&tg.InputMediaWebPage{
-			URL:             config.StartImage,
-			ForceLargeMedia: true,
-		}, &tg.MediaOptions{
-			Caption:     caption,
-			NoForwards:  true,
-			ReplyMarkup: core.GetStartMarkup(m.ChannelID()),
-		})
+		// -------------------------------------------------
+		// START MENU
+		// -------------------------------------------------
+
+		caption := F(
+			m.ChannelID(),
+			"start_private",
+			locales.Arg{
+				"user": utils.MentionHTML(m.Sender),
+				"bot":  utils.MentionHTML(m.Client.Me()),
+			},
+		)
+
+		_, err := m.RespondMedia(
+			&tg.InputMediaWebPage{
+				URL:             config.StartImage,
+				ForceLargeMedia: true,
+			},
+			&tg.MediaOptions{
+				Caption:     caption,
+				NoForwards:  true,
+				ReplyMarkup: core.GetStartMarkup(m.ChannelID()),
+			},
+		)
+
 		if err != nil {
 			gologging.Error(
 				"[start] InputMediaWebPage Reply failed: " + err.Error(),
 			)
 
-			_, err = m.RespondMedia(config.StartImage, &tg.MediaOptions{
-				Caption:     caption,
-				NoForwards:  true,
-				ReplyMarkup: core.GetStartMarkup(m.ChannelID()),
-			})
+			_, err = m.RespondMedia(
+				config.StartImage,
+				&tg.MediaOptions{
+					Caption:     caption,
+					NoForwards:  true,
+					ReplyMarkup: core.GetStartMarkup(m.ChannelID()),
+				},
+			)
+
 			if err != nil {
 				gologging.Error(
 					"[start] URL media reply failed: " + err.Error(),
 				)
 
-				_, err = m.Respond(caption, &tg.SendOptions{
-					NoForwards:  true,
-					ReplyMarkup: core.GetStartMarkup(m.ChannelID()),
-				})
+				_, err = m.Respond(
+					caption,
+					&tg.SendOptions{
+						NoForwards:  true,
+						ReplyMarkup: core.GetStartMarkup(m.ChannelID()),
+					},
+				)
+
 				return err
 			}
 		}
 	}
 
+	// -------------------------------------------------
+	// LOGGER
+	// -------------------------------------------------
+
 	if config.LoggerID != 0 && isLoggerEnabled() {
 		uName := "N/A"
+
 		if m.Sender.Username != "" {
 			uName = "@" + m.Sender.Username
 		}
-		msg := F(m.ChannelID(), "logger_bot_started", locales.Arg{
-			"mention":       utils.MentionHTML(m.Sender),
-			"user_id":       m.SenderID(),
-			"user_username": uName,
-		})
-		_, err := m.Client.SendMessage(config.LoggerID, msg)
+
+		msg := F(
+			m.ChannelID(),
+			"logger_bot_started",
+			locales.Arg{
+				"mention":       utils.MentionHTML(m.Sender),
+				"user_id":       m.SenderID(),
+				"user_username": uName,
+			},
+		)
+
+		_, err := m.Client.SendMessage(
+			config.LoggerID,
+			msg,
+		)
+
 		if err != nil {
 			gologging.Error(
 				"Failed to send logger_bot_started msg, Err: " + err.Error(),
 			)
 		}
 	}
+
 	return tg.ErrEndGroup
 }
 
 func startCB(cb *tg.CallbackQuery) error {
 	cb.Answer("")
 
-	caption := F(cb.ChannelID(), "start_private", locales.Arg{
-		"user": utils.MentionHTML(cb.Sender),
-		"bot":  utils.MentionHTML(cb.Client.Me()),
-	})
+	caption := F(
+		cb.ChannelID(),
+		"start_private",
+		locales.Arg{
+			"user": utils.MentionHTML(cb.Sender),
+			"bot":  utils.MentionHTML(cb.Client.Me()),
+		},
+	)
 
 	sendOpt := &tg.SendOptions{
 		ReplyMarkup: core.GetStartMarkup(cb.ChannelID()),
@@ -157,5 +254,6 @@ func startCB(cb *tg.CallbackQuery) error {
 	}
 
 	cb.Edit(caption, sendOpt)
+
 	return tg.ErrEndGroup
 }
